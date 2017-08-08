6feb262775813bcde4c0b6bbfcc6babd2508d005
candicecz <czech.candice@gmail.com>
2017-08-07 23:35:02 -0400

    changes to config

--- src/config.js
+++ src/config.js
@@ -1,13 +1,12 @@
 module.exports = {
 
-  //API_HOST: "https://private-793ff-dashboardly.apiary-mock.com"
+  API_HOST: "https://private-793ff-dashboardly.apiary-mock.com"
   // "https://private-anon-c3f982c0e0-dashboardly.apiary-mock.com"
 }
 =======
-  API_HOST:` https://private-anon-3599b87f0d-dashboardly.apiary-mock.com`
+  //API_HOST:` https://private-anon-3599b87f0d-dashboardly.apiary-mock.com`
 
 
 
 // "https://private-anon-c3f982c0e0-dashboardly.apiary-mock.com"
 }
-
